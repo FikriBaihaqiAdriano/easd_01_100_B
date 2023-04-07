3f4f9cc6e2afd0399a8a2ad8dcf6b7ff96c2eae8
FikriBaihaqiAdriano <fikribaihaqiadriano@gmail.com>
2023-04-07 10:59:23 +0700

    Add project files.

---
+++ easd_01_100_B/easd_01_100_B.cpp
@@ -0,0 +1,10 @@
+// 1. karena algoritma adalah urutan instruksi yang terstruktur dan sistematis untuk menyelesaikan suatu masalah
+// 2. Array
+//	 Linked List
+// 3. 	A. Compiler
+//		B. Bahasa Pemograman 
+//		C. Ukuran dari inputan
+// 4. quick sort karena dalam kasus terburuknya tetap menghasilkan waktu loglinear O(n log n) dan bisa lebih baik lagi
+// 5. A. Algoritma yang termasuk Quadratic : Bubble Sort, Insertion Sort, Selection sort
+//	  B. Algoritma yang termasuk Loglinear : Quick Sort, Merge sort
+// 6. maaf mas saya bingung belum dapat pemnjelasannya
